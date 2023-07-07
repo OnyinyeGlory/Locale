57b608f83960ded54a549dd6c21f88e827156acf
Uchenna J. Onyenso <alphadev.onyenso@gmail.com>
2023-07-07 21:31:59 +0100

    fix: deployment ish

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -2,10 +2,10 @@ from .base import *
 import dj_database_url
 
 
-DATABASES = {
-    'default': dj_database_url.config(
-        # Feel free to alter this value to suit your needs.
-        default=config("DATABASE_URL"),
-        conn_max_age=600
-    )
-}
+# DATABASES = {
+#     'default': dj_database_url.config(
+#         # Feel free to alter this value to suit your needs.
+#         default=config("DATABASE_URL"),
+#         conn_max_age=600
+#     )
+# }
