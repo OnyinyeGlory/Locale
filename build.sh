2555ae1ba0c482e4c8c708c377657e40eec67781
Uchenna J. Onyenso <alphadev.onyenso@gmail.com>
2023-07-07 20:42:14 +0100

    patch: deployment

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@
 set -o errexit
 
 poetry install
+pip install -r requirements.txt
 python manage.py makemigrations
 python manage.py migrate
 python manage.py populate_database
--- config/settings/dev.py
+++ config/settings/dev.py
@@ -5,7 +5,7 @@ import dj_database_url
 DATABASES = {
     'default': dj_database_url.config(
         # Feel free to alter this value to suit your needs.
-        default='postgres://onyi:Kw2cuWebLaG96X77Pxwk0qdYcIvFUCAc@dpg-cik6g1mnqql0l1o1q3og-a/locale',
+        default=config("DATABASE_URL"),
         conn_max_age=600
     )
 }
